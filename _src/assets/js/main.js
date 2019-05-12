d7f78833ce4bc5e65963acef43e1f6c23018972f
marinarodriguezandres <marinarodriguezandres@gmail.com>
2019-05-12 13:25:09 +0200

    add path testing with other svg

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,29 @@
-'use strict';
+"use strict";
+var distancePerPoint = 7;
+var drawFPS = 60;
 
-console.log('>> Ready :)');
+var orig = document.querySelector("path"),
+  length,
+  timer;
+//orig.addEventListener("mousein", startDrawingPath, false);
+//orig.addEventListener("mouseout", stopDrawingPath, false);
+
+function startDrawingPath() {
+  length = 0;
+  orig.style.stroke = "#ffffff";
+  timer = setInterval(increaseLength, 1000 / drawFPS);
+}
+
+function increaseLength() {
+  var pathLength = orig.getTotalLength();
+  length += distancePerPoint;
+  orig.style.strokeDasharray = [length, pathLength].join(" ");
+  if (length >= pathLength) clearInterval(timer);
+}
+
+//function stopDrawingPath() {
+//  clearInterval(timer);
+//  orig.style.stroke = "";
+//}
+startDrawingPath();
+
